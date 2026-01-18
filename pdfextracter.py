6fe10bf4c49045ff39aca3b8f8ce23a7c7be14c5
ryousuf569 <yousufrashid.coding@gmail.com>
2026-01-18 15:42:41 -0500

    added comments

--- pdfextracter.py
+++ pdfextracter.py
@@ -1,8 +1,10 @@
 from PyPDF2 import *
 
+# Add the path to your pdf here
 reader = PdfReader("path_to_your.pdf")
 writer = PdfWriter()
 
+# Select the number of pages
 for i in range(10, 138):
     writer.add_page(reader.pages[i])
 
