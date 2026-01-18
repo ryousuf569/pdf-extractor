490f8a580987a05d81aec11ccc1c1ebc505adfa2
ryousuf569 <yousufrashid.coding@gmail.com>
2026-01-18 15:43:48 -0500

    fixed some comments

--- pdfextracter.py
+++ pdfextracter.py
@@ -4,9 +4,10 @@ from PyPDF2 import *
 reader = PdfReader("path_to_your.pdf")
 writer = PdfWriter()
 
-# Select the number of pages
+# Select the start and finishing pages
 for i in range(10, 138):
     writer.add_page(reader.pages[i])
 
+# Add the path you want to save it too 
 with open("name of the file", "wb") as f:
     writer.write(f)
